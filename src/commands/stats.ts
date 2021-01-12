baff937352b8b204778a0dea25b41dfc8e24c3fc
Androz2091 <androz2091@gmail.com>
2021-01-12 17:38:52 +0100

    :sparkles: Show job in stats embed

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -2,6 +2,7 @@ import { Snowflake, MessageEmbed } from 'discord.js';
 import { CommandContext, SlashCommand, SlashCreator } from 'slash-create';
 import { client } from '../bot';
 import { getUser } from '../database/models/User';
+import jobs from '../assets/jobs.json';
 
 export default class StatsCommand extends SlashCommand {
     constructor (creator: SlashCreator) {
@@ -28,10 +29,19 @@ export default class StatsCommand extends SlashCommand {
 
         const health = 100;
         const userData = await getUser(userID);
+        const jobData = userData.job ? jobs.find((j) => j.name === userData.job) : null;
 
         const embed = new MessageEmbed()
             .addField(`Money`, isAuthor ? `You currently have **$${userData.money}** at the bank! :moneybag:` : `${user} has currently **$${userData.money}** at the bank! :moneybag:`)
             .addField('Health', isAuthor ? `You are currently **${health}%** healthy! :heart_decoration:` : `${user} is currently **${health}%** healthy! :heart_decoration:`)
+            .addField('Job', 
+                isAuthor ?
+                    jobData ? `You are currently a **${jobData.name}**, paid **$${jobData.salary}** per hour! :man_in_tuxedo:`
+                            : `You currently have no jobs! :man_in_tuxedo:`
+                    :
+                    jobData ? `${user} is currently a **${jobData.name}**, paid **$${jobData.salary}** per hour! :man_in_tuxedo:`
+                            : `${user} currently has no jobs! :man_in_tuxedo:`
+            )
             .setColor('#36393F');
 
         ctx.send(`<@${ctx.member.id}>, here are ${tag}'s stats!`, {
